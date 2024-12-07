60ddceb1033ea4fe57cf2db7b68c1d9971ebb92a
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2024-12-06 17:49:28 -0800

    Implemented score tracking for 5 round game.

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const choices = ["Rock", "Paper", "Scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random()*3)];
     let result = "";
 
     if(playerChoice === computerChoice){
-        result = "It's a draw!";
+        result = "DRAW!";
     }
     else{
         switch(playerChoice){
@@ -28,4 +32,15 @@ function playGame(playerChoice){
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
     resultDisplay.textContent = result;
+
+    switch(result){
+        case "YOU WIN!":
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "YOU LOSE!":
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+    }
 }
